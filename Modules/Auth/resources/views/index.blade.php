88edb18af3b86637f8f2ea9a0877ffa8401543b7
vw-uday <verloop.dev4@gmail.com>
2026-09-01 12:38:08 +0530

    customer login page

--- Modules/Auth/resources/views/index.blade.php
+++ Modules/Auth/resources/views/index.blade.php
@@ -56,7 +56,7 @@
    <div id="page" class="page font--jakarta">
 			<div id="login" class="bg--fixed login-1 login-section division">
 				<div class="container">
-					<div class="row align-items-center justify-content-center">
+					<div class="row align-items-center justify-content-center m-auto">
 						<div class="col-md-6 col-lg-6 col-12">	
 							<div class="register-page-form">
 								<!-- TITLE -->
@@ -106,13 +106,6 @@
 							</div>	
 						</div>	
 
-            <div class="col-md-6 col-lg-6 col-12">
-              <div class="d-flex justify-content-center align-items-start">
-                  <div class="img-block">
-                      <img src="{{ asset('front/images/Login-page-1.png') }}" alt="login now" class="img-fluid">
-                  </div>
-              </div>
-            </div>
 			 		</div>	  <!-- End row -->	
 			 	</div> <!-- End container -->		
 			</div>	<!-- END LOGIN PAGE -->
